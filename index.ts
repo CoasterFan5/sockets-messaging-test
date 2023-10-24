27183a47c0c05ac8ada4224047539d2c1f0f4e60
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2023-10-24 16:33:07 +0000

    Make Messaging Service

---
+++ index.ts
@@ -0,0 +1,21 @@
+import { io } from "socket.io-client";
+
+
+
+const socket = io("https://sockets.hosted.coasterfan5.com")
+
+
+console.log('working...')
+
+socket.on("error", () => {
+    console.log("error")
+})
+
+socket.on("message", (args) => {
+    console.log(args)
+})
+
+socket.on("connect", () => {
+    console.log('connected')
+    socket.emit("message", "Hello World")
+})
